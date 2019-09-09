96bf28225030b134cb04862a27fca8361bc69ac1
UraFIZ <kratser88@gmail.com>
2019-09-09 10:40:54 +0300

    init commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.API.Data;
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = context.Values.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
